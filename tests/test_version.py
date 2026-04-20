435825579a34aa776b83abc981bfe1dd5a8699ef
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2026-04-20 15:20:01 +0300

    Add version string tests

---
+++ tests/test_version.py
@@ -0,0 +1,16 @@
+import re
+
+import skypulse
+
+
+def test_version_is_set():
+    assert skypulse.__version__
+    assert isinstance(skypulse.__version__, str)
+
+
+def test_version_is_semver():
+    assert re.match(r"^\d+\.\d+\.\d+", skypulse.__version__)
+
+
+def test_version_matches_expected():
+    assert skypulse.__version__ == "2.1.0"
